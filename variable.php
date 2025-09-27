cf954c329b5fc9ac1074af69c14e3443450431c9
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-09-27 21:07:00 +0700

    syntax how to show variable

--- variable.php
+++ variable.php
@@ -4,4 +4,7 @@ $var1 = "Adi"; // String
 $var2 = 30; // Integer
 $var3 = 3.14; // float/double
 $var4 = true; // Boolean
+
+// Cara memampilkan variable 
+echo "Selamat Datang di aplikasi kami $var1. Semoga Bermanfaat $var2, $var3, $var4";
 ?>
