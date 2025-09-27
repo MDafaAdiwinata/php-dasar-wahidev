f05a9bb5e1b664f6a355447c435a15769afa399e
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-09-27 21:07:40 +0700

    check jenis variable, function php

--- variable.php
+++ variable.php
@@ -7,4 +7,16 @@ $var4 = true; // Boolean
 
 // Cara memampilkan variable 
 echo "Selamat Datang di aplikasi kami $var1. Semoga Bermanfaat $var2, $var3, $var4";
+
+// Cara check jenis variable
+echo var_dump($var1);
+echo "<br>";
+
+echo var_dump($var2);
+echo "<br>";
+
+echo var_dump($var3);
+echo "<br>";
+
+echo var_dump($var4);
 ?>
