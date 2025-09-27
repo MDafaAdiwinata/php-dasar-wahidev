6db6ccb3bd44b5c96a5f6c2a023e3619bcab9908
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-09-27 21:11:11 +0700

    Operator Komparatif - If Else

--- operator.php
+++ operator.php
@@ -19,4 +19,24 @@ echo "<br> <br> Operator Komparasi <br>";
 echo var_dump($x > $y);
 echo "<br>";
 echo var_dump($x == $y);
+
+// Operator Komparatif (If - Else)
+// 3 Macam (if saja, if else, dan if else - if else)
+echo "<br> <br>";
+echo "Operator Komparatif (If - else) <br>";
+if ( $x > $y ) {
+    echo "nilai $x Lebih Besar dari nilai $y";
+} else if ( $x < $y ) {
+    echo "nilai $x lebih kecil dari nilai $y";
+} else if ( $x >= $y ) {
+    echo "nilai $x lebih besar sama dengan dari nilai $y";
+} else if ( $x <= $y ) {
+    echo "nilai $x lebih kecil sama dengan dari nilai $y";
+} else if ( $x == $y ) {
+    echo "nilai $x sama dengan nilai $y";
+} else if ( $x != $y ) {
+    echo "nilai $x tidak sama dengan nilai $y";
+} else {
+    echo "Tidak ada Nilai";
+}
 ?>
