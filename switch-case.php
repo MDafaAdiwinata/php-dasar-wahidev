86448b51c4015814f7cb9138b9c15f195f53131f
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-09-27 21:14:00 +0700

    add syntax program switch case

--- switch-case.php
+++ switch-case.php
@@ -2,4 +2,18 @@
 // Switch Case
 // Switch Case membutuhkan break untuk setiap case
 
+$lampu = "merah";
+switch ($lampu) {
+    case "merah":
+        echo "Lampu $lampu, bertanda untuk Berhenti";
+        break;
+    case "kuning":
+        echo "Lampu $lampu, bertanda untuk Hati - hati";
+        break;
+    case "hijau":
+        echo "Lampu $lampu, bertanda untuk Jalan";
+        break;
+    default:
+        echo "Lampu Mati";
+}
 ?>
