8e2f4fff1e99351190a9cb36cb811116dc42e1b8
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-09-28 11:25:24 +0700

    Add show array syntax

--- array.php
+++ array.php
@@ -2,4 +2,7 @@
 // # Array
 // Array merupakan variable yang berfungsi untuk menyimpan banyaknya data dalam satu tempat variable
 
+// 1. Menampilkan data array
+echo "Saya pernah mengunjungi kota $kota[0], $kota[1], dan kota $kota[2]";
+echo "<br>";
 ?>
