f8465cf6d15e5329b86e7a7a220cdccaa2e56eb3
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-09-28 11:31:42 +0700

    Add examples all programs looping - for

--- looping.php
+++ looping.php
@@ -4,4 +4,26 @@
 //     code 
 // }
 
+// For Increment
+for ( $i = 1; $i <= 10; $i++ ) {
+    echo "Perulangan For ke - $i <br>";
+}
+
+echo "<br>";
+
+// For Decrement
+for ( $j = 10; $j >= 1; $j-- ) {
+    echo "Perulangan For ke - $j <br>";
+} 
+
+echo "<br>";
+
+// # Perulangan - Array
+$kota = ["Jakarta", "Semarang", "Wonogiri"];
+
+echo "<h2>Kota - kota di Indonesia</h2>";
+for ( $k = 0; $k < count($kota); $k++ ) {
+    echo ($k + 1).". Kota $kota[$k] <br>";
+}
+
 ?>
