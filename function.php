8c80645b9cdad9ec2fc2ca3567b11b711fee84ed
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-09-29 07:21:35 +0700

    Add definition of function

--- function.php
+++ function.php
@@ -1,4 +1,5 @@
 <?php
 // # Function
-
+// Function berguna untuk memproses sesuatu menjadi hal lain, istilahnya dengan mesin
+// Function wajib memberikan return nilai untuk menampilkan hasilnya.
 ?>
