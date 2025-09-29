6f8b51977f594740cae42d64974367768c389668
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-09-29 07:19:01 +0700

    Add array data examples

--- foreach.php
+++ foreach.php
@@ -3,4 +3,7 @@
 // Jenis perulangan yang lebih efisien dan efektif
 // Perulangan hanya untuk menampilkan data, tidak berupa angka
 
+$kota = ["Jakarta", "Tangerang", "Bekasi"];
+$makanan = ["Nasi Padang", "Nasi Goreng", "Mie Goreng"];
+
 ?>
