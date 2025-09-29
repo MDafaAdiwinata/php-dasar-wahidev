ea76bb67a4b5cd512cb5b06d33806c0409939926
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-09-29 07:21:59 +0700

    Add function tanpa parameter

--- function.php
+++ function.php
@@ -2,4 +2,14 @@
 // # Function
 // Function berguna untuk memproses sesuatu menjadi hal lain, istilahnya dengan mesin
 // Function wajib memberikan return nilai untuk menampilkan hasilnya.
+
+// function tanpa parameter
+function Pertambahan() {
+    $x = 10;
+    $y = 20;
+
+    $hasil = $x + $y;
+    return "$x di tambah $y adalah: $hasil";
+}
+echo Pertambahan();
 ?>
